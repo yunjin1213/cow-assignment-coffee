339142c0cb84fe2f25b9eb59dd75640169c2f456
Kim YunJin <yunjinkim25@mju.ac.kr>
2024-09-17 00:06:47 +0900

    조금 수정

--- cafe/Cafe.java
+++ cafe/Cafe.java
@@ -2,34 +2,35 @@ package cafe;
 
 public class Cafe {
 
-    public static void main(String[] args) {
-        CafeManager cafeManager = new CafeManager();
-        cafeManager.startTalking();
+	public static void main(String[] args) {
+		CafeManager cafeManager = new CafeManager();
+		User user = new User(); // User 객체 생성
+		OrderSystem orderSystem = new OrderSystemImpl(user); // OrderSystemImpl 인스턴스 생성
 
-        while (cafeManager.isTalking()) {
-            cafeManager.introduceFunction();
-            int talkNumber = cafeManager.askNumber();
+		cafeManager.startTalking();
 
-            switch (talkNumber) {
-                case 1 -> {
-                    cafeManager.introduceMenu();
-                    int selectMenuNumber = cafeManager.askNumber();
-                    // TODO: OrderSystem 주문 구현 로직 호출
-                }
-                case 2 -> {
+		while (cafeManager.isTalking()) {
+			cafeManager.introduceFunction();
+			int talkNumber = cafeManager.askNumber();
 
-                    // TODO: OrderSystem 주문 취소 구현 로직 호출
-                }
-                case 3 -> {
-                    // TODO: OrderSystem 결제 구현 로직 호출
-                }
-                case 4 -> {
-                    cafeManager.finishTalking();
-                }
-                default -> {
-                    cafeManager.faultValue();
-                }
-            }
-        }
-    }
+			switch (talkNumber) {
+			case 1 -> {
+				cafeManager.introduceMenu();
+				orderSystem.order(); // 주문 처리
+			}
+			case 2 -> {
+				orderSystem.cancelOrder(); // 주문 취소 처리
+			}
+			case 3 -> {
+				orderSystem.pay(); // 결제 처리
+			}
+			case 4 -> {
+				cafeManager.finishTalking(); // 대화 종료
+			}
+			default -> {
+				cafeManager.faultValue(); // 잘못된 입력 처리
+			}
+			}
+		}
+	}
 }
--- cafe/User.java
+++ cafe/User.java
@@ -3,9 +3,27 @@ package cafe;
 import java.util.ArrayList;
 import java.util.List;
 
-public class User { // hint : singleton
+public class User {
 
-    private final List<String> orders = new ArrayList<>();
+    private List<String> orders = new ArrayList<>();  // 주문 목록
 
-    private int money = 25000;
+    // 주문 추가 메서드
+    public void addOrder(String order) {
+        orders.add(order);
+    }
+
+    // 주문 삭제 메서드 (취소 시 사용)
+    public String removeOrder(int index) {
+        return orders.remove(index);
+    }
+
+    // 주문 목록 반환 메서드
+    public List<String> getOrders() {
+        return orders;
+    }
+
+    // 주문 목록 초기화 메서드
+    public void clearOrders() {
+        orders.clear();
+    }
 }
